c6e8135744affa5dc40465dc75fd0698434eba3b
Santosh <78397565+santoshsh2307@users.noreply.github.com>
2023-05-03 20:37:15 +0530

    owner app fc

---
+++ data-services-nova/src/main/java/com/nova/demo/DataServicesNovaApplication.java
@@ -0,0 +1,13 @@
+package com.nova.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServicesNovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataServicesNovaApplication.class, args);
+	}
+
+}
